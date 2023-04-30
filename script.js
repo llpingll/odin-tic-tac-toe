ce711924292a5f4b3ea565c4bc1e12464d724fc3
llpingll <94099281+llpingll@users.noreply.github.com>
2023-04-30 01:00:00 +0100

    Add playRound to control gameflow | Complete draw result functionality

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ const gameBoard = (() => {
 
     const setToken = (e) => {
         const index = e.target.dataset.index;
-        if (_board[index] !== "") {
-            return;
-        }
         _board[index] = gameControl.getActivePlayer().getToken();
     }
 
@@ -41,28 +38,47 @@ const displayControl = (() => {
         }
     }
 
-    // const updateInfo = () => {
+    const setWinnerMessage = (activePlayer) => {
+        $infoElement.textContent = `Player ${activePlayer.getToken()} Wins!`;
+    }
 
-    // }
+    const setDrawMessage = () => {
+        $infoElement.textContent = `It's a draw!`;
+    }
 
-    // Event listener
-    $cells.forEach(elem => {
-        elem.addEventListener("click", (e) => {
-            gameBoard.setToken(e);
-            gameControl.updateActivePlayer();
-            render();
+    const setTurnMessage = (activePlayer) => {
+        $infoElement.textContent = `Player ${activePlayer.getToken()}'s turn`;
+    }
+
+    const addCellEvent = () => {
+        $cells.forEach(elem => {
+            elem.addEventListener("click", (e) => {
+                if (gameBoard.getBoard()[e.target.dataset.index] === "") {
+                    gameControl.playRound(e);
+                    render();
+                }
+            });
         });
-    });
+    }
+
+    const removeCellEvent = () => {
+        $cells.forEach(elem => {
+            elem.removeEventListener("click", () => {});
+        });
+    }
 
     $button.addEventListener("click", () => {
-        gameBoard.restart();
-        gameControl.resetActivePlayer();
+        gameControl.resetGame();
         render();
     });
+
+    return {setTurnMessage, addCellEvent, removeCellEvent, setDrawMessage, setWinnerMessage};
 
 })();
 
 const gameControl = (() => {
+
+    let _round = 0;
     const _players = [];
     _players.push(Player("X"));
     _players.push(Player("O"));
@@ -70,14 +86,73 @@ const gameControl = (() => {
     let _activePlayer = _players[0];
 
     const getActivePlayer = () => _activePlayer;
+
     const updateActivePlayer = () => {
         _activePlayer === _players[0] ? _activePlayer = _players[1] : _activePlayer = _players[0];
     }
-    const resetActivePlayer = () => {
+
+    const resetGame = () => {
+        gameBoard.restart();
+        _round = 1;
         _activePlayer = _players[0];
+        displayControl.setTurnMessage(_activePlayer);
     }
 
-    return {getActivePlayer, updateActivePlayer, resetActivePlayer};
-    // F Toggle active player
-    // F 
+    const checkForWinner = () => {
+        const winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            let a = gameBoard.getBoard()[winCondition[0]];
+            let b = gameBoard.getBoard()[winCondition[1]];
+            let c = gameBoard.getBoard()[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const playRound = (e) => {
+        // Run on ititial call
+        if (_round === 0) {
+            displayControl.addCellEvent();
+            displayControl.setTurnMessage(_activePlayer);
+            _round ++;
+            return;
+        }
+        // Run if rounds end
+        if (_round === 9) {
+            gameBoard.setToken(e);
+            displayControl.removeCellEvent();
+            displayControl.setDrawMessage();
+            return;
+        }
+        // Run if plays available
+        if (checkForWinner() === true) {
+            displayControl.setWinnerMessage(_activePlayer);
+            displayControl.removeCellEvent();
+        }
+        console.log("!");
+        gameBoard.setToken(e);
+        updateActivePlayer();
+        displayControl.setTurnMessage(_activePlayer);
+        _round ++;
+    }
+
+    playRound();
+
+    return {getActivePlayer, updateActivePlayer, resetGame, playRound};
 })();
